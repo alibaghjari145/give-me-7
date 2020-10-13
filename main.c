55ccebc563b7f0bf6792cc59a8ead731bea048eb
ali baghjari <70961525+alibaghjari145@users.noreply.github.com>
2020-10-14 00:26:46 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -3,17 +3,168 @@
 
 int main()
 {
-    printf("* * * * * * * *\n");
-        printf(" * * * * * * * *\n");
-  printf("* * * * * * * *\n");
-        printf(" * * * * * * * *\n");
-  printf("* * * * * * * *\n");
-        printf(" * * * * * * * *\n");
-  printf("* * * * * * * *\n");
-        printf(" * * * * * * * *\n");
-  printf("* * * * * * * *\n");
-        printf(" * * * * * * * *\n");
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+scanf("%d",&d);
+scanf("%d",&e);
+scanf("%d",&f);
+scanf("%d",&g);
 
+if(a==0){
+printf(" \n");
+}else if(a==1){
+printf("* \n");
+}else if(a==2){
+printf("** \n");
+}else if(a==3){
+printf("*** \n");
+}else if(a==4){
+printf("****\n");
+}else if(a==5){
+printf("*****\n");
+}else if(a==6){
+printf("******\n");
+}else if(a==7){
+printf("*******\n");
+}else if(a==8){
+printf("********\n");
+}else if(a==9){
+printf("*********\n");
+}
+if(b==0){
+printf(" \n");
+}else if(b==1){
+printf("* \n");
+}else if(b==2){
+printf("** \n");
+}else if(b==3){
+printf("*** \n");
+}else if(b==4){
+printf("****\n");
+}else if(b==5){
+printf("*****\n");
+}else if(b==6){
+printf("******\n");
+}else if(b==7){
+printf("*******\n");
+}else if(b==8){
+printf("********\n");
+}else if(b==9){
+printf("*********\n");
+}
+if(c==0){
+printf(" \n");
+}else if(c==1){
+printf("* \n");
+}else if(c==2){
+printf("** \n");
+}else if(c==3){
+printf("*** \n");
+}else if(c==4){
+printf("****\n");
+}else if(c==5){
+printf("*****\n");
+}else if(c==6){
+printf("******\n");
+}else if(c==7){
+printf("*******\n");
+}else if(c==8){
+printf("********\n");
+}else if(c==9){
+printf("*********\n");
+}
+if(d==0){
+printf(" \n");
+}else if(d==1){
+printf("* \n");
+}else if(d==2){
+printf("** \n");
+}else if(d==3){
+printf("*** \n");
+}else if(d==4){
+printf("****\n");
+}else if(d==5){
+printf("*****\n");
+}else if(d==6){
+printf("******\n");
+}else if(d==7){
+printf("*******\n");
+}else if(d==8){
+printf("********\n");
+}else if(d==9){
+printf("*********\n");
+}
+if(e==0){
+printf(" \n");
+}else if(e==1){
+printf("* \n");
+}else if(e==2){
+printf("** \n");
+}else if(e==3){
+printf("*** \n");
+}else if(e==4){
+printf("****\n");
+}else if(e==5){
+printf("*****\n");
+}else if(e==6){
+printf("******\n");
+}else if(e==7){
+printf("*******\n");
+}else if(e==8){
+printf("********\n");
+}else if(e==9){
+printf("*********\n");
+}
+if(f==0){
+printf(" \n");
+}else if(f==1){
+printf("* \n");
+}else if(f==2){
+printf("** \n");
+}else if(f==3){
+printf("*** \n");
+}else if(f==4){
+printf("****\n");
+}else if(f==5){
+printf("*****\n");
+}else if(f==6){
+printf("******\n");
+}else if(f==7){
+printf("*******\n");
+}else if(f==8){
+printf("********\n");
+}else if(f==9){
+printf("*********\n");
+}
+if(g==0){
+printf(" \n");
+}else if(g==1){
+printf("* \n");
+}else if(g==2){
+printf("** \n");
+}else if(g==3){
+printf("*** \n");
+}else if(g==4){
+printf("****\n");
+}else if(g==5){
+printf("*****\n");
+}else if(g==6){
+printf("******\n");
+}else if(g==7){
+printf("*******\n");
+}else if(g==8){
+printf("********\n");
+}else if(g==9){
+printf("*********\n");
+}
 return(0);
 }
 
